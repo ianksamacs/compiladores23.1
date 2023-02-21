599321943f977f86780dac96a8c9a72a50dae0d1
duasck <edu0017@live.com>
2023-02-21 20:21:37 -0300

    teste2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {Samac}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+#olá xD
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
